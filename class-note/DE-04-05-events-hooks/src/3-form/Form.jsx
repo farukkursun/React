c7c299106a0e25445b4f87dd58bc49427630783e
faruk <kursunomerfaruk2@gmail.com>
2022-10-20 10:03:39 +0200

    form onsubmit

---
+++ class-note/DE-04-05-events-hooks/src/3-form/Form.jsx
@@ -0,0 +1,78 @@
+// !JSX ile HTML arasında çok fazla fark olmamakla birlikte bazı önemli farklar da mevcut elbette.
+//! HTML'de class olarak tanımlanan nitelik JSX'de className olarak tanımlanmalı.
+//! HTML'de tek kelime olmayan tabindex gibi nitelikler JSX'de camelCase yapısına uygun olarak tabIndex şeklinde dönüştürülür.
+//! HTML'de onclick, onchange gibi nitelikler yukarıdaki camelCase örneğine göre onClick, onChange şeklinde dönüştürülür.
+//! HTML'de label etiketinde belirtilen for attribute ü JSX'de htmlFor olarak tanımlanmalı.
+//! HTML'de value niteliği JSX'de defaultValue olarak tanımlanmalı.
+//! Ayrıca JSX de  javascript ifadeleri {} içinde çalıştırılabilir. Buna map, && gibi yapılarda dahil.
+
+import { useState } from "react";
+
+
+const Form = () => {
+const [ad, setAd] = useState('')
+const [password, setPassword] = useState('')
+const [country, setCountry] = useState('')
+
+const formAlindi = ()=>{
+  alert(
+  `Username: ${ad}
+  Password: ${password}
+  Country: ${country}`
+
+
+
+  )
+}
+
+
+
+  return (
+    <div className="container text-center mt-4">
+<h1>***********************************************************************</h1>
+<h1>FORMS(EVENTS)</h1>
+<form onSubmit={formAlindi}>
+<div className="mb-3">
+    <label htmlFor="username" className="form-label">
+      Username: <span className="text-danger fw-bold">{ad}</span>
+    </label>
+    <input
+      type="text"
+      className="form-control"
+      id="username" 
+      onChange={(e)=>setAd(e.target.value)}
+      placeholder=""
+    />
+  </div>
+  <div className="mb-3">
+    <label htmlFor="password" className="form-label">
+     Password
+    </label>
+    <input
+      type="password"
+      className="form-control"
+      id="password"
+      onChange={(a)=>setPassword(a.target.value)}
+      placeholder=""
+    />
+  </div>
+<div>
+    <div className="mb-3">
+          <label htmlFor="country">Countyr: <span className="text-danger fw-bold">{country}</span> </label>
+          <select className="form-select" name='country' id='country'
+          onChange={(e)=>setCountry(e.target.value)}>
+          
+            <option value='' >Country</option>
+            <option value="türkiye">Türkiye</option>
+            <option value="deutchland">Deutchland</option>
+            <option value="usa">USA</option>
+          </select>
+        </div>
+</div>
+<button type="submit" className="btn btn-primary">GÖNDER(submit)</button>
+</form>
+    </div>
+  );
+};
+
+export default Form
--- class-note/DE-04-05-events-hooks/src/App.js
+++ class-note/DE-04-05-events-hooks/src/App.js
@@ -2,15 +2,17 @@ import Events from "./1-events-hooksintro/Events";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Hooks from "./2-hooks/Hooks";
 import Counter from "./2-classComponents/Counter";
+import Form from "./3-form/Form";
 
 
 function App() {
   return (
     <div>
-     <Events/>
+     {/* <Events/>
      <Hooks/>
      <Counter/>
-     <Counter count={10}/>
+     <Counter count={10}/> */}
+     <Form/>
     </div>
   );
 }
