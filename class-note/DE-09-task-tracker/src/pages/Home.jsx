f560c162a72958474cbef73d088aa49f070ceeae
faruk <kursunomerfaruk2@gmail.com>
2022-10-25 19:30:36 +0200

    neu file added

---
+++ class-note/DE-09-task-tracker/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import GorevEkle from '../components/GorevEkle'
+import GorevleriGoster from '../components/GorevleriGoster'
+
+import Data from "../helper/Data"
+
+const Home = () => {
+
+
+
+
+
+
+  return (
+    <div>
+      <GorevEkle  />
+
+      <GorevleriGoster  />
+    </div>
+  );
+}
+
+export default Home
