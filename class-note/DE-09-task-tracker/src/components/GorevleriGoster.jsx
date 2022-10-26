d0cdf5f6ed6e76a503522babdbee59d1ce9d5dc1
faruk <kursunomerfaruk2@gmail.com>
2022-10-26 13:35:43 +0200

    update

--- class-note/DE-09-task-tracker/src/components/GorevleriGoster.jsx
+++ class-note/DE-09-task-tracker/src/components/GorevleriGoster.jsx
@@ -8,19 +8,19 @@ setGorevler(gorevler.filter((i)=>i.id!==id))
   return (
     <div>
       {gorevler.map((gorev) => {
-        {/* const { id, text, day, bittiMi } = gorev; */}
+        const { id, text, day, bittiMi } = gorev;
         return (
           <div
-            className={gorev.bittiMi ? "trueStil" : "falseStil"}
-            key={gorev.id}
+            className={bittiMi ? "trueStil" : "falseStil"}
+            key={id}
             onDoubleClick={() =>
-              setGorevler(gorevler.map((i)=> i.id===gorev.id ? {...i,bittiMi:!i.bittiMi}:i))
+              setGorevler(gorevler.map((i)=> i.id===id ? {...i,bittiMi:!i.bittiMi}:i))
             }
           >
             <h3>
-              {gorev.text} <FaTimesCircle style={{ color: "red" }} onClick={()=>deleteGorev(gorev.id)}/>
+              {text} <FaTimesCircle style={{ color: "red" }} onClick={()=>deleteGorev(id)}/>
             </h3>
-            <h6>{gorev.day}</h6>
+            <h6>{day}</h6>
           </div>
         );
       })}
