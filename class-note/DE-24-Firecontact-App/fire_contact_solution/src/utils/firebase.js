810df8843e7cb0bb2123b92faae27ce0984996bf
faruk <kursunomerfaruk2@gmail.com>
2022-11-22 14:24:25 +0100

    update env file

--- class-note/DE-24-Firecontact-App/fire_contact_solution/src/utils/firebase.js
+++ class-note/DE-24-Firecontact-App/fire_contact_solution/src/utils/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2y-sfGPFCvGLvr20qbouSDofmjskoAnQ",
-  authDomain: "realtimetadabase.firebaseapp.com",
-  databaseURL: "https://realtimetadabase-default-rtdb.firebaseio.com",
-  projectId: "realtimetadabase",
-  storageBucket: "realtimetadabase.appspot.com",
-  messagingSenderId: "342635223280",
-  appId: "1:342635223280:web:93c9b6e49e810adeba0a35"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
